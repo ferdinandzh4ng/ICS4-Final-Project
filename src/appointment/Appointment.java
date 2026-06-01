16035283d166c1d2ae252b90c16b87ba55411ba5
unknown <luoida94@gmail.com>
2026-06-01 09:34:10 -0400

    made date class and updated Appointment

--- src/appointment/Appointment.java
+++ src/appointment/Appointment.java
@@ -1,4 +1,94 @@
 package appointment;
 
 public class Appointment {
+    private int apptID;
+    private Patient patient;
+    private Staff staff;
+    private Date date;
+    private double time;
+    private double duration;
+    private double cost;
+    private String status;
+
+    //constructor
+    public Appointment(int apptID, Patient patient, Staff staffList, Date date, double time, double duration, double cost, String status) {
+        this.apptID = apptID;
+        this.patient = patient;
+        this.staffList = staffList;
+        this.date = date;
+        this.time = time;
+        this.duration = duration;
+        this.cost = cost;
+        this.status = status;
+    }
+
+    //accessors and mutators
+    public int getApptID() {
+        return apptID;
+    }
+
+    public void setApptID(int apptID) {
+        this.apptID = apptID;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Staff getStaffList() {
+        return staffList;
+    }
+
+    public void setStaffList(Staff staffList) {
+        this.staffList = staffList;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+    
+
 }
--- src/shared/Date.java
+++ src/shared/Date.java
@@ -1,4 +1,115 @@
 package shared;
 
 public class Date {
+    private int year;
+    private int month;
+    private int day;
+
+    //constructor
+    public Date(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public Date(String dateStr) {
+        String[] parts = dateStr.split(" ");
+        switch(parts[0]) {
+            case "January":
+                this.month = 01;
+                break;
+            case "February":
+                this.month = 02;
+                break;
+            case "March":
+                this.month = 03;
+                break;
+            case "April":
+                this.month = 04;
+                break;
+            case "May":
+                this.month = 05;
+                break;
+            case "June":
+                this.month = 06;
+                break;
+            case "July":
+                this.month = 07;
+                break;
+            case "August":
+                this.month = 08;
+                break;
+            case "September":
+                this.month = 09;
+                break;
+            case "October":
+                this.month = 10;
+                break;
+            case "November":
+                this.month = 11;
+                break;
+            case "December":
+                this.month = 12;
+                break;
+        }
+        this.year = Integer.parseInt(parts[2]);
+        this.day = Integer.parseInt(parts[1]);
+    }
+
+@override
+    public String toString() {
+        String monthStr = "";
+        switch(this.month) {
+            case 01:
+                monthStr = "January";
+                break;
+            case 02:
+                monthStr = "February";
+                break;
+            case 03:
+                monthStr = "March";
+                break;
+            case 04:
+                monthStr = "April";
+                break;
+            case 05:
+                monthStr = "May";
+                break;
+            case 06:
+                monthStr = "June";
+                break;
+            case 07:
+                monthStr = "July";
+                break;
+            case 08:
+                monthStr = "August";
+                break;
+            case 09:
+                monthStr = "September";
+                break;
+            case 10:
+                monthStr = "October";
+                break;
+            case 11:
+                monthStr = "November";
+                break;
+            case 12:
+                monthStr = "December";
+                break;
+        }
+        return monthStr + " " + this.day + ", " + this.year;
+
+    }
+
+    public int compareTo(Date other) {
+        if(this.year != other.year) {
+            return this.year - other.year;
+        } else if(this.month != other.month) {
+            return this.month - other.month;
+        } else {
+            return this.day - other.day;
+        }
+    }
+
+
 }
